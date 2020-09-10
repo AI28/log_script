8656f819e759fb3d76bed08cb86db1c61d23ab56
Alexandru Ichim <aIchim@yonder.local>
2020-09-10 14:59:59 +0300

    process_usage - added cli argument for directory path

--- process_usage.sh
+++ process_usage.sh
@@ -1,65 +1,64 @@
 #!/bin/bash
 
 logs=('process' 'disk' 'memory' 'load')
+date=`date | awk '{print $3, $2, $6, $4}'`
 
 create_file(){
-    file="$1.log"
     if [[ -f $file ]];
     then
         echo "file exists."
     else
-        touch "/var/log/systemusage/$file"
+        touch $1
     fi 
 }
+
+#to do == implementare cu top
 process_logs(){
     create_file $1
-    `ps -aux --sort=-pcpu | head -n 6 | awk {'print $11, $2, $3'} >> /var/log/systemusage/process.log` 
-
+    echo $date>>$1
+    ps -aux --sort=-pcpu | head -n 6 | tail -n 5 | awk {'print $11, $2, $3'} >> $1
 }
 
 disk_logs(){
     create_file $1
-    `df | awk '{print $1, $5, $6}' >> /var/log/systemusage/disk.log`
+    echo $date>>$1 
+    df | tail -n +2 | awk '{print $1, $5, $6}' >> $1
 }
 
 memory_logs(){
-    create_file $1
-    date=`date | awk '{print $3, $2, $6, $4}' `
+    create_file $1 
     mem_usage=`free | grep "Mem" | awk '{print $2, $3, $4, $5, $6}'`
-    `echo "$date $mem_usage" >> /var/log/systemusage/memory.log`
+    echo "$date $mem_usage" >> $1
 }
 
 load_logs(){
-    create_file $1
+    create_file $1 
     loadavg=`cut -d" " -f 1,2,3 /proc/loadavg`
-    date=`date | awk '{print $3, $2, $6, $4}'`
-    `echo "$date $loadavg" >> /var/log/systemusage/load.log`
+    echo "$date $loadavg" >> $1
 }
 
 get_logs(){
     case $1 in
         process)
-           process_logs "process" 
+           process_logs "$2/process.log" 
            ;;
         disk)
-            disk_logs "disk"
+            disk_logs "$2/disk.log" 
             ;;
         memory)
-            memory_logs "memory"
+            memory_logs "$2/memory.log" 
             ;;
         load)
-            load_logs "load"
+            load_logs "$2/load.log" 
             ;;
         *)
             echo "Please enter a valid argument."
             echo "$1"
             ;;
     esac
-
 }
-
 
 for t in ${logs[@]};
 do
-    get_logs $t
+    get_logs $t $1
 done
