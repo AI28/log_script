24455a2cc3150ce01f9ba193c33b2ce2f3426b89
Alexandru Ichim <aIchim@yonder.local>
2020-09-10 13:29:20 +0300

    quick fix

--- process_usage.sh
+++ process_usage.sh
@@ -13,27 +13,27 @@ create_file(){
 }
 process_logs(){
     create_file $1
-    `ps -aux --sort=-pcpu | head -n 6 | awk {'print $11, $2, $3'} > /var/log/systemusage/process.log` 
+    `ps -aux --sort=-pcpu | head -n 6 | awk {'print $11, $2, $3'} >> /var/log/systemusage/process.log` 
 
 }
 
 disk_logs(){
     create_file $1
-    `df | awk '{print $1, $5, $6}' > /var/log/systemusage/disk.log`
+    `df | awk '{print $1, $5, $6}' >> /var/log/systemusage/disk.log`
 }
 
 memory_logs(){
     create_file $1
     date=`date | awk '{print $3, $2, $6, $4}' `
     mem_usage=`free | grep "Mem" | awk '{print $2, $3, $4, $5, $6}'`
-    `echo "$date $mem_usage" > /var/log/systemusage/memory.log`
+    `echo "$date $mem_usage" >> /var/log/systemusage/memory.log`
 }
 
 load_logs(){
     create_file $1
     loadavg=`cut -d" " -f 1,2,3 /proc/loadavg`
     date=`date | awk '{print $3, $2, $6, $4}'`
-    `echo "$date $loadavg" > /var/log/systemusage/load.log`
+    `echo "$date $loadavg" >> /var/log/systemusage/load.log`
 }
 
 get_logs(){
